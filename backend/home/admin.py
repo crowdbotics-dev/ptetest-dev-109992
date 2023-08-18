a59e1519ba9f7522c486f8b6a135391c39e2e609
crowdbotics-dev <engineering@crowdbotics.com>
2023-08-18 14:23:50 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Xswe
+admin.site.register(Xswe)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,6 @@
+from django.conf import settings
 from django.db import models
+class Xswe(models.Model):
+    'Generated Model'
+    hfjf = models.BigIntegerField()
+    teur = models.BigIntegerField()
